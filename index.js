3919083374ed5954970899a29f25d5f681487a0f
LEELAVATHI D H <68585471+leelavathidh192@users.noreply.github.com>
2023-02-17 12:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const variable = "hello world"
-if (variable === "hello world") {
+const variable = "hello world!"
+if (variable === "hello world!") {
     console.log("hii welcome to git")
 } else {
     console.log("not reachable to git")
